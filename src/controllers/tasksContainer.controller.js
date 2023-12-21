1214ed8dca34acdb80e03647e9e9a4da1f3f50fc
flavius <kete102@gmail.com>
2023-12-21 13:10:05 +0100

    Added toastify

--- src/controllers/tasksContainer.controller.js
+++ src/controllers/tasksContainer.controller.js
@@ -1,6 +1,7 @@
 import view from "../components/tasksContainer.html";
 import Task from "./task.controller";
 import "../css/tasksContainer.css";
+import Toastify from "toastify-js";
 
 export default () => {
   const divElement = document.createElement("div");
@@ -25,14 +26,28 @@ export default () => {
     toggleDropDownMenu();
   });
 
+  /*Add new task from form data*/
   const addTaskForm = divElement.querySelector(".dropdown-form");
   addTaskForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const { name, description, priority, category } = Object.fromEntries(
       new FormData(e.target),
     );
+    localStorage.setItem("1", Task(name, description, priority, category));
+    console.log(localStorage.getItem(1));
     tasksWrapper.appendChild(Task(name, description, priority, category));
     addTaskForm.reset();
+    toggleDropDownMenu();
+    Toastify({
+      text: "New Task Added",
+      className: "info-task-added",
+      duration: 3000,
+      gravity: "bottom",
+      position: "right",
+      style: {
+        background: "linear-gradient(to right, #ff5f6d, #ffc371)",
+      },
+    }).showToast();
   });
 
   return divElement;
